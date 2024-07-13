db0c289c0d0977e18840d86bd4ee4248eb5c8a0d
Dilshad <hmydilshadkt@gmail.com>
2024-07-13 16:23:19 +0530

    fix

--- utils/verifyToken.ts
+++ utils/verifyToken.ts
@@ -2,11 +2,15 @@ import { API_URL } from "@/constants";
 import axios from "axios";
 export const verifyToken = async (token: any) => {
   try {
-    const res = await axios.post(`${API_URL}auth/home`, token, {
-      headers: {
-        "X-auth-token": `Bearer ${token.value}`,
-      },
-    });
+    const res = await axios.post(
+      `https://project-management-tk72.onrender.com/api/auth/home`,
+      token,
+      {
+        headers: {
+          "X-auth-token": `Bearer ${token.value}`,
+        },
+      }
+    );
     return res.data;
   } catch (error: any) {
     return { status: false, error: error.response.data };
