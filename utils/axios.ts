300389d77a7217bf0ef84d3776efc9aaa9207bdd
Dilshad <hmydilshadkt@gmail.com>
2024-07-13 12:45:43 +0530

    feat : change axios config

--- utils/axios.ts
+++ utils/axios.ts
@@ -3,14 +3,18 @@ import axios from "axios";
 
 // Create an Axios instance
 const instance = axios.create({
-  baseURL: "https://project-management-tk72.onrender.com/api/",
-  // baseURL: `http://localhost:8080/api/`,
+  // baseURL: "https://project-management-tk72.onrender.com/api/",
+  baseURL: `http://localhost:8080/api/`,
   withCredentials: true,
 });
 
 // Interceptor to handle any additional configurations
 instance.interceptors.request.use(
   (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["X-auth-token"] = `Bearer ${token}`;
+    }
     return config;
   },
   (error) => {
