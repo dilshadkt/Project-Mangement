b34f396ea3f97cef6c43fa1b9a5acf23b0dc445c
Dilshad <hmydilshadkt@gmail.com>
2024-07-13 12:32:59 +0530

    feat : toto crud

--- middleware.tsx
+++ middleware.tsx
@@ -3,32 +3,32 @@ import { NextRequest, NextResponse } from "next/server";
 import { verifyToken } from "./utils/verifyToken";
 
 export async function middleware(request: NextRequest) {
-  const cookieStore = cookies();
-  const token = cookieStore.get("token");
-  const protectedRoutes = ["/", "/stick-wall", "/upcoming", "/calendar"];
+  // const cookieStore = cookies();
+  // const token = cookieStore.get("token");
+  // const protectedRoutes = ["/", "/stick-wall", "/upcoming", "/calendar"];
 
-  // Check if the current path is protected
-  const isProtectedRoute = protectedRoutes.includes(request.nextUrl.pathname);
+  // // Check if the current path is protected
+  // const isProtectedRoute = protectedRoutes.includes(request.nextUrl.pathname);
 
-  // If no token and the route is protected, redirect to login
-  if (!token && isProtectedRoute) {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
-  }
+  // // If no token and the route is protected, redirect to login
+  // if (!token && isProtectedRoute) {
+  //   return NextResponse.redirect(new URL("/auth/login", request.url));
+  // }
 
-  // If token is present, verify it
-  if (token) {
-    const response = await verifyToken(token);
+  // // If token is present, verify it
+  // if (token) {
+  //   const response = await verifyToken(token);
 
-    // If token is invalid and the route is protected, redirect to login
-    if (!response.status && isProtectedRoute) {
-      return NextResponse.redirect(new URL("/auth/login", request.url));
-    }
+  //   // If token is invalid and the route is protected, redirect to login
+  //   if (!response.status && isProtectedRoute) {
+  //     return NextResponse.redirect(new URL("/auth/login", request.url));
+  //   }
 
-    // If token is valid and the request is for auth routes, redirect to dashboard
-    if (response.status && request.nextUrl.pathname.startsWith("/auth")) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-  }
+  //   // If token is valid and the request is for auth routes, redirect to dashboard
+  //   if (response.status && request.nextUrl.pathname.startsWith("/auth")) {
+  //     return NextResponse.redirect(new URL("/", request.url));
+  //   }
+  // }
 
   // Proceed to the next middleware or the requested resource
   return NextResponse.next();
